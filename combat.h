85d85b4b1b29712e15564c6a1115e0eb3d90e03b
Edoardo <904807@stud.unive.it>
2026-01-13 17:30:51 +0100

    Combattimento con il Signore Oscuro

---
+++ combat.h
@@ -0,0 +1,6 @@
+#ifndef COMBAT_H
+#define COMBAT_H
+
+int scontro_SignoreOscuro();
+
+#endif
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "cheats.h"
 #include "menu.h"
+#include <time.h>
+#include "combat.h"
+#include <stdlib.h>
 //gameloop
 //gestione del gioco
 
@@ -17,6 +20,15 @@ int main(){
     if(konami(valorekonami)){
         menuprincipaletrucchi();
     }
+
+    srand(time(NULL));
+    int risultato_scontro = scontro_SignoreOscuro();
+
+    if(risultato_scontro == 1){  //Vittoria dell'eroe
+        return 0;
+    }else{
+        menuprincipale(); //In caso di sconfitta si ritorna al menu principale
+    }
+
     return 0;
 }
-
